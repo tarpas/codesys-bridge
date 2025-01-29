5a812b6a7e41600f4800a84d3243b5e55c5e3714
tarpas <tibor.arpas@infinit.sk>
2025-01-29 15:51:17 +0100

    Functional export to text file. It exports to ../projectname_git/st_source

--- git_support.py
+++ git_support.py
@@ -1,4 +1,3 @@
-
 # encoding:utf-8
 # We enable the new python 3 print syntax
 from __future__ import print_function
@@ -6,7 +5,8 @@ import os
 import shutil
 import time
 import subprocess
-'''
+
+"""
 prop_method		= Guid('792f2eb6-721e-4e64-ba20-bc98351056db')
 tp				= Guid('2db5746d-d284-4425-9f7f-2663a34b0ebc') #dut
 libm			= Guid('adb5cb65-8e1d-4a00-b70a-375ea27582f3')
@@ -37,12 +37,13 @@ ImagePool			=Guid('bb0b9044-714e-4614-ad3e-33cbdf34d16b')
 Project_Information	=Guid('085afe48-c5d8-4ea5-ab0d-b35701fa6009')
 SoftMotion_General_Axis_Pool=Guid('e9159722-55bc-49e5-8034-fbd278ef718f')
 
-'''
+"""
 
 print("--- Saving files in the project: ---")
 
-# git 
-has_repo=False
+# git
+has_repo = False
+
 
 # Get project path and set save folder to st_source subdirectory
 project_path = projects.primary.path
@@ -50,50 +51,52 @@ parent_dir = os.path.dirname(os.path.dirname(project_path))  # Go up one more le
 save_folder = os.path.join(parent_dir, os.path.splitext(os.path.basename(project_path))[0]+"_git", 'st_source')
 
 if not os.path.exists(save_folder):
-	os.makedirs(save_folder) 
+    os.makedirs(save_folder)
 else:
-	#非空文件夹 删除多余
-	a=os.listdir(save_folder)
-	for f in a:
-		if not f.startswith("."): #保留 svn,git 目录
-			sub_path= os.path.join(save_folder,f)
-			if os.path.isdir(sub_path):
-				shutil.rmtree(sub_path)
-			else:
-				os.remove(sub_path)
-		elif f==".git":
-			has_repo=True
+    a = os.listdir(save_folder)
+    for f in a:
+        if not f.startswith("."):
+            sub_path = os.path.join(save_folder, f)
+            if os.path.isdir(sub_path):
+                shutil.rmtree(sub_path)
+            else:
+                os.remove(sub_path)
+        elif f == ".git":
+            has_repo = True
 
-info={}
+info = {}
 
-type_dist={
-'792f2eb6-721e-4e64-ba20-bc98351056db':'pm',  #property method
-'2db5746d-d284-4425-9f7f-2663a34b0ebc':'dut',  #dut
-'adb5cb65-8e1d-4a00-b70a-375ea27582f3':'lib',  #lib manager
-'f89f7675-27f1-46b3-8abb-b7da8e774ffd':'m', 	 #method no ret
-'8ac092e5-3128-4e26-9e7e-11016c6684f2':'act',  #action 
-'6f9dac99-8de1-4efc-8465-68ac443b7d08':'pou',  #pou
-'6654496c-404d-479a-aad2-8551054e5f1e':'itf',  #interface 
-'738bea1e-99bb-4f04-90bb-a7a567e74e3a':'',	   # folder
-'ffbfa93a-b94d-45fc-a329-229860183b1d':'gvl',  #global var
-'5a3b8626-d3e9-4f37-98b5-66420063d91e':'prop', #property
-'2bef0454-1bd3-412a-ac2c-af0f31dbc40f':'tl',   #textlist
-'63784cbb-9ba0-45e6-9d69-babf3f040511':'gtl',  #global textlist
-'225bfe47-7336-4dbc-9419-4105a7c831fa':'dev',	 #device
-'ae1de277-a207-4a28-9efb-456c06bd52f3':'tc',   #task configuration
-'f8a58466-d7f6-439f-bbb8-d4600e41d099':'m',    #method with ret
-'261bd6e6-249c-4232-bb6f-84c2fbeef430':'gvl',   #gvl_Persistent
-'98a2708a-9b18-4f31-82ed-a1465b24fa2d':'task'
-};
+type_dist = {
+    "792f2eb6-721e-4e64-ba20-bc98351056db": "pm",  # property method
+    "2db5746d-d284-4425-9f7f-2663a34b0ebc": "dut",  # dut
+    "adb5cb65-8e1d-4a00-b70a-375ea27582f3": "lib",  # lib manager
+    "f89f7675-27f1-46b3-8abb-b7da8e774ffd": "m",  # method no ret
+    "8ac092e5-3128-4e26-9e7e-11016c6684f2": "act",  # action
+    "6f9dac99-8de1-4efc-8465-68ac443b7d08": "pou",  # pou
+    "6654496c-404d-479a-aad2-8551054e5f1e": "itf",  # interface
+    "738bea1e-99bb-4f04-90bb-a7a567e74e3a": "",  # folder
+    "ffbfa93a-b94d-45fc-a329-229860183b1d": "gvl",  # global var
+    "5a3b8626-d3e9-4f37-98b5-66420063d91e": "prop",  # property
+    "2bef0454-1bd3-412a-ac2c-af0f31dbc40f": "tl",  # textlist
+    "63784cbb-9ba0-45e6-9d69-babf3f040511": "gtl",  # global textlist
+    "225bfe47-7336-4dbc-9419-4105a7c831fa": "dev",  # device
+    "ae1de277-a207-4a28-9efb-456c06bd52f3": "tc",  # task configuration
+    "f8a58466-d7f6-439f-bbb8-d4600e41d099": "m",  # method with ret
+    "261bd6e6-249c-4232-bb6f-84c2fbeef430": "gvl",  # gvl_Persistent
+    "98a2708a-9b18-4f31-82ed-a1465b24fa2d": "task",
+}
 
-def save(text,path,name,tp):
-	if not tp:
-		tp=''
-	else:
-		tp='.st'
-	with open(os.path.join(path,name+tp),'w') as f:
-		f.write(text.encode('utf-8'))
-'''
+
+def save(text, path, name, tp):
+    if not tp:
+        tp = ""
+    else:
+        tp = ".st"
+    with open(os.path.join(path, name + tp), "w") as f:
+        f.write(text.encode("utf-8"))
+
+
+"""
 def get_mtype(a):
 	b=a.text
 	b=b.split("\n")
@@ -116,101 +119,100 @@ def get_mtype(a):
 		elif a.find("VAR_GLOBAL")>=0 or a.find("VAR_CONFIG") >=0:
 			return 'gvl'
 	return ""
-'''			
-	
-def print_tree(treeobj, depth,path):
-	global info
-	#record current Path
-	curpath=path 
-	isfolder=False
-	
-	t='' #text
-	tp='' #type
-	
-	# get object name
-	name = treeobj.get_name(False)
-	id = treeobj.type.ToString()
-	
-	if id in type_dist:
-		tp = type_dist[treeobj.type.ToString()]
-	else:
-		info[id]=name
-		
-		
-	if treeobj.is_device:
-		deviceid = treeobj.get_device_identification()
-		t = 'type='+str(deviceid.type) +'\nid=' +str(deviceid.id) + '\nver='+ str(deviceid.version)
+"""
 
-	try:
-		if treeobj.is_folder :
-			#system.ui.prompt('folder:'+u, PromptChoice.YesNo, PromptResult.Yes)
-			isfolder=true
-			pass
-	except:
-		pass
 
-	if treeobj.has_textual_declaration :
-		t=t+'(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n'
-		a=treeobj.textual_declaration
-		t=t+a.text
-		
-	if treeobj.has_textual_implementation:
-		t=t+'(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n'
-		a=treeobj.textual_implementation
-		t=t+a.text
-		
-	'''	
+def print_tree(treeobj, depth, path):
+    global info
+    # record current Path
+    curpath = path
+    isfolder = False
+
+    t = ""  # text
+    tp = ""  # type
+
+    # get object name
+    name = treeobj.get_name(False)
+    id = treeobj.type.ToString()
+
+    if id in type_dist:
+        tp = type_dist[treeobj.type.ToString()]
+    else:
+        info[id] = name
+
+    if treeobj.is_device:
+        deviceid = treeobj.get_device_identification()
+        t = (
+            "type="
+            + str(deviceid.type)
+            + "\nid="
+            + str(deviceid.id)
+            + "\nver="
+            + str(deviceid.version)
+        )
+
+    try:
+        if treeobj.is_folder:
+            # system.ui.prompt('folder:'+u, PromptChoice.YesNo, PromptResult.Yes)
+            isfolder = true
+            pass
+    except:
+        pass
+
+    if treeobj.has_textual_declaration:
+        t = t + "(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
+        a = treeobj.textual_declaration
+        t = t + a.text
+
+    if treeobj.has_textual_implementation:
+        t = (
+            t
+            + "(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
+        )
+        a = treeobj.textual_implementation
+        t = t + a.text
+
+    """	
 	if treeobj.is_task_configuration:
 		exports=[treeobj]
 		projects.primary.export_native(exports,os.path.join(curpath,name+'.tc'))
 		
-	'''
-	
-	if treeobj.is_task :
-		exports=[treeobj]
-		projects.primary.export_native(exports,os.path.join(curpath,name+'.task'))
-	
-		
-	if treeobj.is_libman:
-		exports=[treeobj]
-		projects.primary.export_native(exports,os.path.join(curpath,name+'.lib'))
-	
-	if treeobj.is_textlist:
-		treeobj.export(os.path.join(curpath,name+'.tl'))
-		
-	children = treeobj.get_children(False)
+	"""
 
-	if children or isfolder:
-		if tp:
-			curpath=os.path.join(curpath,name+'.'+tp)
-		else:
-			curpath=os.path.join(curpath,name)
-		
-		if not os.path.exists(curpath):
-			os.makedirs(curpath)
-			
-	if t:
-		save(t,curpath,name,tp)
+    if treeobj.is_task:
+        exports = [treeobj]
+        projects.primary.export_native(exports, os.path.join(curpath, name + ".task"))
 
-	for child in treeobj.get_children(False):
-		print_tree(child, depth+1,curpath)
-		
+    if treeobj.is_libman:
+        exports = [treeobj]
+        projects.primary.export_native(exports, os.path.join(curpath, name + ".lib"))
+
+    if treeobj.is_textlist:
+        treeobj.export(os.path.join(curpath, name + ".tl"))
+
+    children = treeobj.get_children(False)
+
+    if children or isfolder:
+        if tp:
+            curpath = os.path.join(curpath, name + "." + tp)
+        else:
+            curpath = os.path.join(curpath, name)
+
+        if not os.path.exists(curpath):
+            os.makedirs(curpath)
+
+    if t:
+        save(t, curpath, name, tp)
+
+    for child in treeobj.get_children(False):
+        print_tree(child, depth + 1, curpath)
+
 
 for obj in projects.primary.get_children():
-    print_tree(obj,0,save_folder)
+    print_tree(obj, 0, save_folder)
 
-project_svn = projects.primary.svn
-
-print("start update")
-version_summary = project_svn.get_version_summary()
-print("version_summary.min_version: {}".format(version_summary.min_version))
-project_svn.update()
-print("end update")
-
-print("dir(projects): {} {}".format(projects.primary, dir(project_svn)))
-
-with open(os.path.join(save_folder,'s.txt'),'w') as f:
-	f.write(str(info))
+with open(os.path.join(save_folder, "s.txt"), "w") as f:
+    f.write(str(info))
 
 # if has_repo:
 # 	os.chdir(save_folder)
@@ -227,13 +229,4 @@ with open(os.path.join(save_folder,'s.txt'),'w') as f:
 
 # 	subprocess.call('"D:\\Program Files\\Git\\bin\\git.exe" commit -m "'+time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time()))+'"', startupinfo=si)
 print("--- Script finished. ---")
-
-
-def update_project_svn():
-	def set_username(req):
-		req.username = username
-		req.password = password
-		req.save = True # Optional
-
-	svn.auth_username_password += set_username
-	svn.update()
+system.ui.info("save ok")
