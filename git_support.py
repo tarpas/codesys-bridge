ca7ef44915622b70300f8d88d5c98d20af1fd860
tarpas <tibor.arpas@infinit.sk>
2025-01-29 16:53:18 +0100

    Remove types from comments and put them all to dictioray.

--- git_support.py
+++ git_support.py
@@ -7,40 +7,7 @@ import time
 import subprocess
 import scriptengine # type: ignore
 
-"""
-prop_method		= Guid('792f2eb6-721e-4e64-ba20-bc98351056db')
-tp				= Guid('2db5746d-d284-4425-9f7f-2663a34b0ebc') #dut
-libm			= Guid('adb5cb65-8e1d-4a00-b70a-375ea27582f3')
-method_no_ret	= Guid('f89f7675-27f1-46b3-8abb-b7da8e774ffd')
-act				= Guid('8ac092e5-3128-4e26-9e7e-11016c6684f2')
-fb				= Guid('6f9dac99-8de1-4efc-8465-68ac443b7d08')
-itf				= Guid('6654496c-404d-479a-aad2-8551054e5f1e')
-folder			= Guid('738bea1e-99bb-4f04-90bb-a7a567e74e3a')
-gvl				= Guid('ffbfa93a-b94d-45fc-a329-229860183b1d')
-prop			= Guid('5a3b8626-d3e9-4f37-98b5-66420063d91e')
-textlist		= Guid('2bef0454-1bd3-412a-ac2c-af0f31dbc40f')
-global_textlist	= Guid('63784cbb-9ba0-45e6-9d69-babf3f040511')
-Device			= Guid('225bfe47-7336-4dbc-9419-4105a7c831fa')
-task_config		= Guid('ae1de277-a207-4a28-9efb-456c06bd52f3')
-method			= Guid('f8a58466-d7f6-439f-bbb8-d4600e41d099')
-gvl_Persistent	= Guid('261bd6e6-249c-4232-bb6f-84c2fbeef430')
-Project_Settings	=Guid('8753fe6f-4a22-4320-8103-e553c4fc8e04')
-Plc_Logic			=Guid('40b404f9-e5dc-42c6-907f-c89f4a517386')
-Application			=Guid('639b491f-5557-464c-af91-1471bac9f549')
-Task				=Guid('98a2708a-9b18-4f31-82ed-a1465b24fa2d')
-Task_pou			=Guid('413e2a7d-adb1-4d2c-be29-6ae6e4fab820')
-Visualization		=Guid('f18bec89-9fef-401d-9953-2f11739a6808')
-Visualization_Manager=Guid('4d3fdb8f-ab50-4c35-9d3a-d4bb9bb9a628')
-TargetVisualization	=Guid('bc63f5fa-d286-4786-994e-7b27e4f97bd5')
-WebVisualization	=Guid('0fdbf158-1ae0-47d9-9269-cd84be308e9d')
-__VisualizationStyle=Guid('8e687a04-7ca7-42d3-be06-fcbda676c5ef')
-ImagePool			=Guid('bb0b9044-714e-4614-ad3e-33cbdf34d16b')
-Project_Information	=Guid('085afe48-c5d8-4ea5-ab0d-b35701fa6009')
-SoftMotion_General_Axis_Pool=Guid('e9159722-55bc-49e5-8034-fbd278ef718f')
-
-"""
-
-print("--- Saving files in the project: ---")
+print("Start export!")
 
 # git
 has_repo = False
@@ -89,6 +56,18 @@ type_dist = {
     "f8a58466-d7f6-439f-bbb8-d4600e41d099": "m",  # method with ret
     "261bd6e6-249c-4232-bb6f-84c2fbeef430": "gvl",  # gvl_Persistent
     "98a2708a-9b18-4f31-82ed-a1465b24fa2d": "task",
+    "8753fe6f-4a22-4320-8103-e553c4fc8e04": "ps",   # Project Settings
+    "40b404f9-e5dc-42c6-907f-c89f4a517386": "pl",   # PLC Logic
+    "639b491f-5557-464c-af91-1471bac9f549": "app",  # Application
+    "413e2a7d-adb1-4d2c-be29-6ae6e4fab820": "tpou", # Task POU
+    "f18bec89-9fef-401d-9953-2f11739a6808": "vis",  # Visualization
+    "4d3fdb8f-ab50-4c35-9d3a-d4bb9bb9a628": "vm",   # Visualization Manager
+    "bc63f5fa-d286-4786-994e-7b27e4f97bd5": "tv",   # Target Visualization
+    "0fdbf158-1ae0-47d9-9269-cd84be308e9d": "wv",   # Web Visualization
+    "8e687a04-7ca7-42d3-be06-fcbda676c5ef": "vs",   # Visualization Style
+    "bb0b9044-714e-4614-ad3e-33cbdf34d16b": "ip",   # Image Pool
+    "085afe48-c5d8-4ea5-ab0d-b35701fa6009": "pi",   # Project Information
+    "e9159722-55bc-49e5-8034-fbd278ef718f": "smap", # SoftMotion General Axis Pool
 }
 
 
@@ -165,14 +144,14 @@ def print_tree(treeobj, depth, path):
         pass
 
     if treeobj.has_textual_declaration:
-        t = t + "(*#-#-#-#-#-#-#-#-#-#---Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
+        t = t + "(*#-#-#-#-#-#-#-#-#-#---" + tp + " Declaration---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
         a = treeobj.textual_declaration
         t = t + a.text
 
     if treeobj.has_textual_implementation:
         t = (
             t
-            + "(*#-#-#-#-#-#-#-#-#-#---Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
+            + "(*#-#-#-#-#-#-#-#-#-#---" + tp + " Implementation---#-#-#-#-#-#-#-#-#-#-#-#-#*)\r\n"
         )
         a = treeobj.textual_implementation
         t = t + a.text
@@ -233,5 +212,4 @@ with open(os.path.join(save_folder, "s.txt"), "w") as f:
 # 	subprocess.call('"D:\\Program Files\\Git\\bin\\git.exe" add .', startupinfo=si)
 
 # 	subprocess.call('"D:\\Program Files\\Git\\bin\\git.exe" commit -m "'+time.strftime('%Y-%m-%d %H:%M',time.localtime(time.time()))+'"', startupinfo=si)
-print("--- Script finished. ---")
-system.ui.info("save ok")
+print("Export OK.")
